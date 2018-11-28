92e71be9af101379ed125e7fb796ce5ac341d69f
tronthebest <43419856+tronthebest@users.noreply.github.com>
2018-11-28 16:54:33 -0600

    Create work.h

---
+++ work.h
@@ -0,0 +1,5 @@
+I attended 7 labs
+lab 1 was the best one 
+lab 5 was the worst
+the learning objective of both was to teach us how to work with ADT's and teach us how to enter our github and navigate through it
+the obstacles i found were the errors which were made they were difficult to find
